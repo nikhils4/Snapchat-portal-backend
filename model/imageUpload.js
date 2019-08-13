1699b0b014d04ccd9044ba71afb5fa03c0ad798a
Nikhil Singh <nikhilsingh498@gmail.com>
2019-08-13 05:38:00 +0530

    Add cloudinary setup for image upload

---
+++ model/imageUpload.js
@@ -0,0 +1,24 @@
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+
+
+console.log("This is API", process.env.CLOUD_NAME)
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  folder: 'snapchat',
+  allowedFormats: ['jpg', 'png', 'jpeg'],
+  transformation: [{ width: 500, height: 500, crop: 'limit' }],
+});
+
+const parser = multer({ storage });
+
+module.exports.parser = parser;
