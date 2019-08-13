d0ee18ac035b7af3de40ec8e5a9085142bd27493
Nikhil Singh <nikhilsingh498@gmail.com>
2019-08-13 05:33:10 +0530

    Add api,js

---
+++ api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/auth.js');
+const middleware = require('./model/middleware.js');
+const protectedRoutes = require('./routes/protected.js');
+
+require('dotenv').config();
+require('./model/connect.js');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.use(cors());
+// app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+
+app.use(middleware.session);
+
+app.use('/secure', protectedRoutes);
+
+app.listen(port, () => {});
